622a5cd91b3d383f69a8bb333db55471a9bda175
Sophia Smith <153287160+amulets-mending@users.noreply.github.com>
2024-01-10 16:45:43 +0700

    Create transaction.h

---
+++ transaction.h
@@ -0,0 +1,10 @@
+// transaction.h
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include "account.h"
+
+void performTransaction(struct Account *account, double amount);
+
+#endif // TRANSACTION_H
